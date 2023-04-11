4d531f51225ba77627e3a64a8e066c46109a4f05
dev-soumyadeep <105239142+dev-soumyadeep@users.noreply.github.com>
2023-04-11 17:09:00 +0530

    Update digital verification.sol

--- digital verification.sol
+++ digital verification.sol
@@ -1,55 +1,76 @@
+//This takes data and a security question as input and makes a hash of it. Now any time the data needs to be opened that security
+//qstn needs to be entered in order to generate the same hash. If the hash matches with the hash stored in hashes map then it 
+//fetch the information of the owner of that document from mappeddocuments.
+
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+contract digitalVerification{
+   bytes32 hash;
+   bytes32 s_hash;
+   string data;
+   // s_ stands for sumbitted
+   string s_data;
+   string qstn;
+   string s_qstn;
+   mapping(bytes32=>bool)hashes;
+   mapping(bytes32=>bool)checkedHashes;
+   mapping(address=>bytes32 [])mappeddocuments;
+    event Document_Received (address issuing_authority);
+    event Security_Qstn_set (address student);
+   function receiveData(string memory _data) public
+   {
+       data=_data;
+       emit Document_Received (msg.sender);
 
-contract digitalVerification {
-    bytes32 hash;
-    bytes32 s_hash;
-    string data;
-    // s_ stands for sumbitted
-    string s_data;
-    string qstn;
-    string s_qstn;
-    mapping(bytes32 => bool) hashes;
-    mapping(bytes32 => bool) checkedHashes;
-    event Document_Received(address issuing_authority);
-    event Security_Qstn_set(address student);
+   }
+   function receiveQstn(string memory _qstn) public
+   {
+       qstn=_qstn;
+       emit Security_Qstn_set (msg.sender);
 
-    function receiveData(string memory _data) public {
-        data = _data;
-        emit Document_Received(msg.sender);
+   }
+   string a = "huhuhuhuhu";
+   string b = "erererrere";
+
+   function generateHash() public {
+    
+   hash = keccak256(abi.encodePacked(data,qstn)) ;
+   hashes[hash]=true;
+   uint l = mappeddocuments[msg.sender].length;
+  mappeddocuments[msg.sender][l-1]=hash;
+
     }
-
-    function receiveQstn(string memory _qstn) public {
-        qstn = _qstn;
-        emit Security_Qstn_set(msg.sender);
-    }
-
-    function generateHash() public {
-        hash = keccak256(abi.encodePacked(data, qstn));
-        hashes[hash] = true;
-    }
-
-    function getGeneratedHash() public view returns (bytes32) {
+    function getGeneratedHash() public view returns(bytes32)
+    {
         return hash;
-    }
 
-    function s_receiveData(string memory _s_data) public {
-        s_data = _s_data;
     }
-
-    function s_receiveQstn(string memory _s_qstn) public {
-        s_qstn = _s_qstn;
+   
+   function s_receiveData(string memory _s_data) public
+   {
+       s_data=_s_data;
     }
+   function s_receiveQstn(string memory _s_qstn) public
+   {
+       s_qstn=_s_qstn;
 
-    function generate_s_Hash() public {
-        s_hash = keccak256(abi.encodePacked(s_data, s_qstn));
-        bool val = hashes[s_hash];
-        checkedHashes[s_hash] = val;
-    }
+   }
+   function generate_s_Hash() public  {
+       s_hash = keccak256(abi.encodePacked(s_data,s_qstn));
+       bool val = hashes[s_hash];
+       checkedHashes[s_hash]=val; 
 
-    function getSubmittedHash() public view returns (bytes32) {
+
+   }
+     function getSubmittedHash() public view returns(bytes32)
+    {
         return s_hash;
     }
+   function isVerified(bytes32 ) public view returns(bool)
+   {
+      
+   }
+   
 
-    function isVerified(bytes32) public view returns (bool) {}
 }
